a0899ee227b7ee610e86a5e09d48b096f12e7e60
gianbartlo <gianbartolo2001@gmail.com>
2021-02-12 17:40:19 +0100

    Task 1.2

---
+++ pcghome/Assets/Scripts/PlaneGenerator.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneGenerator : MonoBehaviour
+{
+
+    Mesh mesh;
+
+    public Vector3[] vertices;
+    public int[] triangles;
+    Material m_Material;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        CreateShape();
+        UpdateShape();
+        m_Material = GetComponent<Renderer>().material;
+        m_Material.color = Color.red;
+    }
+
+    void CreateShape(){
+
+        vertices = new Vector3[]
+        {
+            new Vector3(0,0,0),
+            new Vector3(0,0,1),
+            new Vector3(1,0,0),
+            new Vector3(1,0,1)
+        };
+
+        triangles = new int[]
+        {
+            0,1,2,
+            1,3,2
+        };
+
+    }
+
+    void UpdateShape(){
+
+        mesh.Clear();
+
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
+    }
+}
---
+++ pcghome/Assets/Scripts/PyramidGenerator.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PyramidGenerator : MonoBehaviour
+{
+
+    Mesh mesh;
+
+    public Vector3[] vertices;
+    public int[] triangles;
+    Material m_Material;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        CreateShape();
+        UpdateShape();
+        m_Material = GetComponent<Renderer>().material;
+        m_Material.color = Color.blue;
+    }
+
+    void CreateShape(){
+
+        vertices = new Vector3[]
+        {
+            new Vector3(-1.0f, 0.0f, 0.0f),
+            new Vector3(0.0f, 0.0f, -1.5f),
+            new Vector3(1.0f, 0.0f, 0.0f),
+            new Vector3(0.0f, 1.0f, -0.8f)
+
+        };
+
+        triangles = new int[]
+        {
+            0, 1, 2,
+            0,3,1,
+            1,3,2,
+            2,3,0
+        };
+
+    }
+
+    void UpdateShape(){
+
+        mesh.Clear();
+
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
+    }
+}
